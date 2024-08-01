736514ca7b0d6afdb996dfaab2d34fb14d5d50fe
rKaushalya <ravinduk56@gmail.com>
2024-08-01 20:09:44 +0530

    do babel configuration and update dependencies

--- react_native_frontend/babel.config.js
+++ react_native_frontend/babel.config.js
@@ -1,13 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ['module-resolver', {
-      root: ['./src'],
-      alias: {
-        '@components': './src/components',
-        '@assets': './src/assets',
-        '@utils': './src/utils'
-      }
-    }]
-  ]
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+  ],
 };
